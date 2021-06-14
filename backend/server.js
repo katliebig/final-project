7c9c535c82a35ff3dccba0f7bb30651f3bf3dfe5
katliebig <liebig.kat@gmail.com>
2021-06-14 12:30:33 +0200

    try to fix cors error

--- backend/server.js
+++ backend/server.js
@@ -99,6 +99,11 @@ const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send(listEndpoints(app))
 })
