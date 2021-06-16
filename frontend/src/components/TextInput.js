9a37b6aa030e3c4af75f0f0e6730fd601027ac00
Erika Andersson Porath <erika.porath@hotmail.com>
2021-06-17 00:44:38 +0200

    Started styling the character sheet, mobile first

--- frontend/src/components/TextInput.js
+++ frontend/src/components/TextInput.js
@@ -4,7 +4,7 @@ const TextInput = ({ onInputChange, label, value }) => {
   return (
     <>
       <label htmlFor={label} >{label}</label>
-      <input type="text" value={value} id={label} onChange={(e) => onInputChange(e.target.value, e.target.id)} />
+      <input className="text-input" type="text" value={value} id={label} onChange={(e) => onInputChange(e.target.value, e.target.id)} />
     </>
   )
 }
