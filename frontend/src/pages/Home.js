f225f31783e3ff9fc29cfd7aede6bd44ba03c050
Erika Andersson Porath <erika.porath@hotmail.com>
2021-06-22 01:10:23 +0200

    Updated the race cards with new images. Moved the disabled card to an upcoming section. Updated the imaged on the home cards and changed the into text a little bit

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,27 +6,22 @@ const Home = () => {
   return (
     <section className="main">
       <h2>Welcome to the character creator!</h2>
-      <p className="main-text">Register and log in to get access to these cool features!</p>
+      <p className="main-text">This is an image creator mainly aimed to create DnD characters. Register and log in to get access to these cool features!</p>
       <div className="cards-container">
         <HomeCard
           imgUrl="../assets/races-screenshot.png"
           title="Races"
-          text="A page with an overview of all the races you can choose from"
+          text="A page with an overview of all the races you can choose from. With a simple click you can create your chosen race in the creator."
         />
         <HomeCard
-          imgUrl="../assets/character-example.png"
+          imgUrl="../assets/character-creator.png"
           title="Character creator"
-          text="A character creator where you can save your images to your gallery"
+          text="A character creator where you can create your character and then save your them to your gallery."
         />
-        {/* <HomeCard
-          imgUrl="../assets/character-example.png"
-          title="Gallery"
-          text="A gallery with all the characters created by all the users"
-        /> */}
         <HomeCard
-          imgUrl="../assets/character-example.png"
+          imgUrl="../assets/user-gallery.png"
           title="User gallery"
-          text="Your personal gallery where you store all your characters and are able to delete them"
+          text="Your personal gallery where you store all your characters and are able to delete them and fill in an character sheet."
         />
       </div>
     </section>
