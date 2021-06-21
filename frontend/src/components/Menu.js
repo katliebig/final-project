963bcae0f859abfbbfc3a350d3a901dab179000f
katliebig <liebig.kat@gmail.com>
2021-06-21 12:53:26 +0200

    hamburger menu closes after clicking on a link

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -6,13 +6,14 @@ import { StyledMenu } from './Menu.styled';
 
 import user from '../reducers/user'
 
-const Menu = ({ open }) => {
+const Menu = ({ open, setOpen }) => {
   const accessToken = useSelector(store => store.user.accessToken)
 
   const dispatch = useDispatch()
   const location = useLocation()
 
   const onLogoutButtonClick = () => {
+    setOpen(false)
     batch(() => {
       dispatch(user.actions.setId(null))
       dispatch(user.actions.setUsername(null))
@@ -24,77 +25,77 @@ const Menu = ({ open }) => {
   return (
     <StyledMenu open={open}>
       <div className="burger-menu-links-container">
-          <Link to="/">
+        <Link to="/" onClick={() => setOpen(false)}>
+          <div className="link-container">
+            <div className="burger-menu-icon-border">
+              <img src="./assets/home.svg" className="header-icon" alt="home icon" />
+            </div>
+            <span className={`link ${location.pathname === "/" ? "current-route" : ""}`}>Home</span>
+          </div>
+        </Link>
+
+        {accessToken &&
+          <Link to="/Race" onClick={() => setOpen(false)}>
             <div className="link-container">
               <div className="burger-menu-icon-border">
-                <img src="./assets/home.svg" className="header-icon" alt="home icon" />
+                <img src="./assets/race.svg" className="header-icon" alt="race icon" />
               </div>
-              <span className={`link ${location.pathname === "/" ? "current-route" : ""}`}>Home</span>
+              <span className={`link ${location.pathname === "/Race" ? "current-route" : ""}`}>Races</span>
             </div>
           </Link>
+        }
 
-          {accessToken &&
-            <Link to="/Race">
-              <div className="link-container">
-                <div className="burger-menu-icon-border">
-                  <img src="./assets/race.svg" className="header-icon" alt="race icon" />
-                </div>
-                <span className={`link ${location.pathname === "/Race" ? "current-route" : ""}`}>Races</span>
-              </div>
-            </Link>
-          }
-
-          {accessToken &&
-            <Link to="/Creator">
-              <div className="link-container">
-                <div className="burger-menu-icon-border">
-                  <img src="./assets/creator.svg" className="header-icon" alt="home icon" />
-                </div>
-                <span className={`link ${location.pathname === "/Creator" ? "current-route" : ""}`}>Creator</span>
-              </div>
-            </Link>
-          }
-
-          <Link to="/Gallery">
+        {accessToken &&
+          <Link to="/Creator" onClick={() => setOpen(false)}>
             <div className="link-container">
               <div className="burger-menu-icon-border">
-                <img src="./assets/gallery.svg" className="header-icon" alt="home icon" />
+                <img src="./assets/creator.svg" className="header-icon" alt="home icon" />
               </div>
-              <span className={`link ${location.pathname === "/Gallery" ? "current-route" : ""}`}>Gallery</span>
+              <span className={`link ${location.pathname === "/Creator" ? "current-route" : ""}`}>Creator</span>
             </div>
           </Link>
+        }
 
-          {accessToken &&
-            <Link to="/UserGallery">
-              <div className="link-container">
-                <div className="burger-menu-icon-border">
-                  <img src="./assets/user.svg" className="header-icon" alt="home icon" />
-                </div>
-                <span className={`link ${location.pathname === "/UserGallery" ? "current-route" : ""}`}>User Gallery</span>
-              </div>
-            </Link>
-          }
+        <Link to="/Gallery" onClick={() => setOpen(false)}>
+          <div className="link-container">
+            <div className="burger-menu-icon-border">
+              <img src="./assets/gallery.svg" className="header-icon" alt="home icon" />
+            </div>
+            <span className={`link ${location.pathname === "/Gallery" ? "current-route" : ""}`}>Gallery</span>
+          </div>
+        </Link>
 
-          {!accessToken &&
-            <Link to="/Login">
-              <div className="link-container">
-                <div className="burger-menu-icon-border"><img src="./assets/login.svg" className="header-icon" alt="home icon" /></div>
-                <span className="link">Log in</span>
+        {accessToken &&
+          <Link to="/UserGallery" onClick={() => setOpen(false)}>
+            <div className="link-container">
+              <div className="burger-menu-icon-border">
+                <img src="./assets/user.svg" className="header-icon" alt="home icon" />
               </div>
-            </Link>
-          }
+              <span className={`link ${location.pathname === "/UserGallery" ? "current-route" : ""}`}>User Gallery</span>
+            </div>
+          </Link>
+        }
 
-          {accessToken &&
-            <Link to="/" onClick={onLogoutButtonClick} className="logout-button">
-              <div className="link-container">
-                <div className="burger-menu-icon-border">
-                  <img src="./assets/logout.svg" className="header-icon" alt="home icon" />
-                </div>
-                <span className="link">Log out</span>
+        {!accessToken &&
+          <Link to="/Login" onClick={() => setOpen(false)}>
+            <div className="link-container">
+              <div className="burger-menu-icon-border"><img src="./assets/login.svg" className="header-icon" alt="home icon" /></div>
+              <span className="link">Log in</span>
+            </div>
+          </Link>
+        }
+
+        {accessToken &&
+          <Link to="/" onClick={onLogoutButtonClick} className="logout-button">
+            <div className="link-container">
+              <div className="burger-menu-icon-border">
+                <img src="./assets/logout.svg" className="header-icon" alt="home icon" />
               </div>
-            </Link>
-          }
-        </div>
+              <span className="link">Log out</span>
+            </div>
+          </Link>
+        }
+      </div>
     </StyledMenu>
   )
 }
