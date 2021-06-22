81ea829ed485e3c67f8bc9c0cee8482ae3662803
katliebig <liebig.kat@gmail.com>
2021-06-22 15:52:51 +0200

    changed login image to slide show with all race images

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch, batch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
+import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 
 import user from '../reducers/user'
 
@@ -100,7 +102,24 @@ const Login = () => {
             disabled={password.length < 8 ? true : false}
           >Sign up</button>
         </form>
-        <img src="./assets/dice.jpg" alt="placeholder" />
+        <Carousel
+          className="login-image-container"
+          showStatus={false}
+          showThumbs={false}
+          showIndicators={false}
+          showArrows={false}
+          swipeable={false}
+          autoPlay={true}
+          infiniteLoop={true}
+          interval={4000}
+        >
+          <img id="login-image" src="./assets/elf.png" alt="elf" />
+          <img id="login-image" src="./assets/tiefling.png" alt="tiefling" />
+          <img id="login-image" src="./assets/human.png" alt="human" />
+          <img id="login-image" src="./assets/dwarf.png" alt="dwarf" />
+          <img id="login-image" src="./assets/human1.png" alt="human" />
+          <img id="login-image" src="./assets/tiefling1.png" alt="tiefling" />
+        </Carousel>
       </div>
     </section>
   )
