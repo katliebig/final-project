be9cbfea722d7c6f5529232739f44950f108e3a7
katliebig <liebig.kat@gmail.com>
2021-06-28 10:46:20 +0200

    added not found page

---
+++ frontend/src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const NotFound = () => {
+  return (
+    <section className="main">
+      <h3>404 - Page not found</h3>
+    </section>
+  )
+}
+
+export default NotFound
