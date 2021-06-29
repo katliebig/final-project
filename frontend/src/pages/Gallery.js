0a1318788e4c9ec8316b606a8c2ab0d752cc3f79
katliebig <liebig.kat@gmail.com>
2021-06-29 21:07:28 +0200

    added pagination for gallery page, not responsive yet

--- frontend/src/pages/Gallery.js
+++ frontend/src/pages/Gallery.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
+import ReactPaginate from 'react-paginate';
 
 import characters from '../reducers/characters'
 import { API_URL } from '../reusables/urls'
@@ -11,12 +12,14 @@ import Loader from '../components/Loader'
 const Gallery = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [filter, setFilter] = useState("")
-  const charactersArray = useSelector(store => store.characters.characters)
+  const [page, setPage] = useState(0)
+  const charactersArray = useSelector(store => store.characters.characters.docs)
+  const meta = useSelector(store => store.characters.characters)
 
   const dispatch = useDispatch()
 
   useEffect(() => {
-    fetch(API_URL(`characters?race=${filter}`))
+    fetch(API_URL(`characters?race=${filter}&page=${page}`))
       .then(res => res.json())
       .then(data => {
         if (data.success) {
@@ -26,14 +29,28 @@ const Gallery = () => {
           alert(data.message)
         }
       })
-  }, [dispatch, filter])
+  }, [dispatch, filter, page])
 
   return (
     <section className="main">
+      {meta && <ReactPaginate
+        previousLabel={<img src="./assets/left.svg" alt="left arrow icon" className="pagination-icon" />}
+        nextLabel={<img src="./assets/right.svg" alt="right arrow icon" className="pagination-icon" />}
+        breakLabel={'...'}
+        breakClassName={'break-me'}
+        pageCount={meta.totalPages}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={3}
+        onPageChange={(currentPage) => setPage(currentPage.selected)}
+        containerClassName={'pagination'}
+        activeClassName={'active'}
+        previousLinkClassName={"pagination-link"}
+        nextLinkClassName={"pagination-link"}
+      />}
       {isLoading && <Loader />}
       {!isLoading && (
         <>
-          <GalleryFilter setFilter={setFilter} />
+          <GalleryFilter setFilter={setFilter} setPage={setPage} />
           <div className="cards-container">
             {charactersArray.map(character => (
               <GalleryCard character={character} key={character._id} />
