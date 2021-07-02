94c9f074825e5bcfee06b8b4a9afe97bc0893d30
katliebig <liebig.kat@gmail.com>
2021-07-02 12:18:49 +0200

    updated FAQ text

--- frontend/src/components/Accordion.js
+++ frontend/src/components/Accordion.js
@@ -18,7 +18,7 @@ const Accordion = () => {
       </Collapsible>
       <Collapsible trigger="How does it work?">
         <p>
-          In the "Character creator" tap you'll find the creator. There you can create your character by chosing a race, and then what 
+          In the "Character creator" tap you'll find the creator. There you can create your character by chosing a race, and then what
           attributes you want to change. Use the arrow buttons to switch between the different images. If you log in you'll be able to
           save your character in the gallery. There you also have a personal gallery where you can also fill in a character sheet for
           your characters. And you are of course free to delete them whenever you want.
@@ -26,22 +26,22 @@ const Accordion = () => {
       </Collapsible>
       <Collapsible trigger="Who made this?">
         <p>
-          This site was made as the final project of Technigos Frontend bootcamp, by Katharina Liebig and Erika Andersson Porath. 
+          This site was made as the final project of Technigo's Frontend Development bootcamp, by Katharina Liebig and Erika Andersson Porath.
         </p>
       </Collapsible>
       <Collapsible trigger="Who made the images?">
         <p>
-          The images for the creator was made by Erika Andersson Porath, and the images on the cards is from seekpng.com and toppng.com. 
-          This might change in the future. 
+          The images for the creator was made by Erika Andersson Porath, and the images on the cards is from seekpng.com and toppng.com.
+          This might change in the future.
         </p>
       </Collapsible>
       <Collapsible trigger="Will the page be updated?">
-      <p>
-        Yes, the're will probably be additions to the attributes in the creator and hopefully have more races added.
-      </p>
-    </Collapsible>
+        <p>
+          Yes, there will probably be additions to the attributes in the creator and hopefully have more races added.
+        </p>
+      </Collapsible>
     </div>
-  );
-};
+  )
+}
 
 export default Accordion
